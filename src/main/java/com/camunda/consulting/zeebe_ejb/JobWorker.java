529368514228afcfe08aee6d3d07cf04f3064abe
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2023-02-07 09:42:24 +0100

    handover 1

---
+++ src/main/java/com/camunda/consulting/zeebe_ejb/JobWorker.java
@@ -0,0 +1,17 @@
+package com.camunda.consulting.zeebe_ejb;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.*;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD })
+public @interface JobWorker {
+  
+  String taskType();
+
+}
--- src/main/java/com/camunda/consulting/zeebe_ejb/ZeebeEjbProcessApplication.java
+++ src/main/java/com/camunda/consulting/zeebe_ejb/ZeebeEjbProcessApplication.java
@@ -2,10 +2,13 @@ package com.camunda.consulting.zeebe_ejb;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.enterprise.inject.spi.BeanManager;
 import javax.inject.Inject;
 
 import org.slf4j.Logger;
@@ -17,6 +20,7 @@ import com.camunda.consulting.zeebe_ejb.worker.DeductCreditHandler;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.Topology;
+import io.camunda.zeebe.client.api.worker.JobHandler;
 
 @Singleton
 @Startup
@@ -27,6 +31,9 @@ public class ZeebeEjbProcessApplication {
   public static ZeebeClient zeebeClient;
   
   @Inject
+  BeanManager beanManager;
+  
+  @Inject
   DeductCreditHandler deductCreditHandler;
   
   @Inject
@@ -34,7 +41,29 @@ public class ZeebeEjbProcessApplication {
   
   @PostConstruct
   public void start() {
-    startWorker();
+//    startWorker();
+    registerWorkers();
+  }
+  
+  private void registerWorkers() {
+    LOG.info("Register workers");
+    beanManager.createInstance().select(JobHandler.class).forEach(handler -> {
+      Optional.ofNullable(handler.getClass().getAnnotation(JobWorker.class)).ifPresent(annotation -> {
+        createWorker(handler, annotation);
+      });
+    });
+  }
+
+  private void createWorker(JobHandler handler, JobWorker annotation) {
+    LOG.info("Register handler {} for type {}", handler, annotation.taskType());
+    zeebeClient
+        .newWorker()
+        .jobType(annotation.taskType())
+        .handler(handler)
+        .timeout(Duration.ofSeconds(10))
+        .requestTimeout(Duration.ofSeconds(30))
+        .open();
+    LOG.info("Worker is open");
   }
 
   private void startWorker() {
@@ -48,20 +77,20 @@ public class ZeebeEjbProcessApplication {
     
     LOG.info("Deployed processes: {}", deploymentEvent.getProcesses());
     
-    zeebeClient
-        .newWorker()
-        .jobType("creditDeduction")
-        .handler(deductCreditHandler)
-        .timeout(Duration.of(10, ChronoUnit.SECONDS))
-        .requestTimeout(Duration.of(30, ChronoUnit.SECONDS))
-        .open();
-    zeebeClient
-        .newWorker()
-        .jobType("creditCardCharging")
-        .handler(chargeCreditCardHandler)
-        .timeout(Duration.of(10, ChronoUnit.SECONDS))
-        .requestTimeout(Duration.of(30, ChronoUnit.SECONDS))
-        .open();
+//    zeebeClient
+//        .newWorker()
+//        .jobType("creditDeduction")
+//        .handler(deductCreditHandler)
+//        .timeout(Duration.of(10, ChronoUnit.SECONDS))
+//        .requestTimeout(Duration.of(30, ChronoUnit.SECONDS))
+//        .open();
+//    zeebeClient
+//        .newWorker()
+//        .jobType("creditCardCharging")
+//        .handler(chargeCreditCardHandler)
+//        .timeout(Duration.of(10, ChronoUnit.SECONDS))
+//        .requestTimeout(Duration.of(30, ChronoUnit.SECONDS))
+//        .open();
     
     LOG.info("worker is open");
   }
