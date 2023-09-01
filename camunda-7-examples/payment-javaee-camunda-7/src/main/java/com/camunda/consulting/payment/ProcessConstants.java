16620078c33ea8d7fae76b46b3aff307a6329fdd
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2023-09-01 11:33:33 +0200

    Clean up the pom files and remove original Camunda 7 example

--- camunda-7-examples/payment-javaee-camunda-7/src/main/java/com/camunda/consulting/payment/ProcessConstants.java
+++
@@ -1,7 +0,0 @@
-package com.camunda.consulting.payment;
-
-public class ProcessConstants {
-
-  public static final String PROCESS_DEFINITION_KEY = "payment-with-java-class"; // BPMN Process ID
-
-}
